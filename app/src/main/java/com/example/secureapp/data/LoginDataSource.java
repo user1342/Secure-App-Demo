406060020ea057188ab9871b3d678a89dce39702
James Stevenson <28961606+user1342@users.noreply.github.com>
2023-03-05 21:28:22 +0000

    Added login options
    Option to login as test and admin

--- app/src/main/java/com/example/secureapp/data/LoginDataSource.java
+++ app/src/main/java/com/example/secureapp/data/LoginDataSource.java
@@ -1,8 +1,13 @@
 package com.example.secureapp.data;
 
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.example.secureapp.data.model.LoggedInUser;
 
 import java.io.IOException;
+import java.util.Objects;
 
 /**
  * Class that handles authentication w/ login credentials and retrieves user information.
@@ -11,6 +16,29 @@ public class LoginDataSource {
 
     public Result<LoggedInUser> login(String username, String password) {
 
+        // Log in as test user, password is 'testtest'
+        if (Objects.equals(username, "test") && Objects.equals(password, "testtest")){
+            Log.v("","");
+
+            LoggedInUser loggedInUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            username);
+            return new Result.Success<>(loggedInUser);
+
+        // Log in as admin user, password is a random UUID. Requires dynamic instrumentation / patching.
+        }else if (Objects.equals(username, "admin") && Objects.equals(password, java.util.UUID.randomUUID().toString())){
+
+            LoggedInUser loggedInUser =
+                    new LoggedInUser(
+                            java.util.UUID.randomUUID().toString(),
+                            username);
+            return new Result.Success<>(loggedInUser);
+
+        }
+
+        return new Result.Error(new IOException("Incorrect Credentials"));
+        /*
         try {
             // TODO: handle loggedInUser authentication
             LoggedInUser fakeUser =
@@ -21,6 +49,8 @@ public class LoginDataSource {
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
         }
+        */
+
     }
 
     public void logout() {
