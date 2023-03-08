5e328d546b7bb77f78fc0cbafea985e94699a013
Nathan R <nathan.rutherford@protonmail.com>
2023-03-08 19:12:00 +0000

    Add a hash to the signature preference.
    Not really working right now, but the logic is there. You can see the updated preference value on a refresh, but want this to be when save it clicked.

--- app/src/main/cpp/secureapp-lib.cpp
+++ app/src/main/cpp/secureapp-lib.cpp
@@ -1,3 +1,5 @@
+#include <jni.h>
+
 // Write C++ code here.
 //
 // Do not forget to dynamically load the C++ library into your application.
@@ -15,3 +17,18 @@
 //         System.loadLibrary("secureapp")
 //      }
 //    }
+extern "C"
+JNIEXPORT jint JNICALL
+Java_com_example_secureapp_SettingsActivity_00024SettingsFragment_calculateSignatureHash(
+        JNIEnv *env, jobject thiz, jstring signature) {
+    char buff[128];
+    const char *str = (*env).GetStringUTFChars(signature,0);
+
+    unsigned long hash = 5381;
+    int c;
+
+    while (c = *str++)
+        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+
+    return hash;
+}
--- app/src/main/java/com/example/secureapp/SettingsActivity.java
+++ app/src/main/java/com/example/secureapp/SettingsActivity.java
@@ -1,9 +1,12 @@
 package com.example.secureapp;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.EditTextPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -25,9 +28,21 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+        protected native int calculateSignatureHash(String signature);
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+            Preference signaturePreference = findPreference("signature");
+            signaturePreference.setOnPreferenceChangeListener((preference, newValue) -> {
+                if (preference.getKey().equals("signature")) {
+                    int signatureHash = calculateSignatureHash(newValue.toString());
+                    String signature = newValue + " <" + signatureHash + ">";
+                    Toast.makeText(this.getContext(), "Signature: " + signature, Toast.LENGTH_SHORT).show();
+                    preference.getSharedPreferences().edit().putString("signature", signature).apply();
+                    return false;
+                }
+                return true;
+            });
         }
     }
 }
